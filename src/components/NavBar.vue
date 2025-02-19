1f07399c5a8cc6393395f3470a9191c49003fe2a
Aditya Dahake <aditya.vd@magicworkshop.ai>
2025-02-19 11:54:52 +0000

    add log out button in home navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,28 @@
-<script setup>
-import { RouterLink } from 'vue-router'
-import ThemeToggle from '@/components/ThemeToggle.vue'
-</script>
-
 <template>
-  <nav class="flex items-center">
+  <nav class="flex items-center gap-3">
     <RouterLink to="/" class="hover:text-primary text-xl font-black">ToDo</RouterLink>
     <ThemeToggle class="ms-auto" />
+    <Button
+      v-if="$route.name === 'home'"
+      @click="handleSignout"
+      size="small"
+      class="text-sm font-semibold"
+    >
+      <i class="pi pi-sign-out !text-sm"></i>Log Out
+    </Button>
   </nav>
 </template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { signOut } from 'aws-amplify/auth'
+
+import router from '@/router'
+import ThemeToggle from '@/components/ThemeToggle.vue'
+import Button from 'primevue/button'
+
+const handleSignout = async () => {
+  await signOut()
+  router.push('/login')
+}
+</script>
